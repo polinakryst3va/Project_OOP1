67c6be068f24a35f9a3c8ffb1c92dfc39115dd38
Polina <polina.kr.0711@gmail.com>
2024-03-14 20:10:19 +0200

    builder class

--- src/NFA.java
+++ src/NFA.java
@@ -1,27 +1,65 @@
-import java.util.Set;
-import java.util.Map;
+import java.util.*;
 
 public class NFA {
+
+    private int id;
     private Set<Integer> states;  //състояния
     private Set<Character> alphabet; //азбука
     private Map<Integer, Map<Character, Set<Integer>>> transitions; //преходи между състояния
-    private Set<Integer> startStates; //начални състояния
+    private int startStates; //начални състояния
     private Set<Integer> finalStates; //крайни състояния
 
-    public NFA(Set<Integer> states, Set<Character> alphabet, Map<Integer, Map<Character, Set<Integer>>> transitions, Set<Integer> startStates, Set<Integer> finalStates) {
-        this.states = states;
-        this.alphabet = alphabet;
-        this.transitions = transitions;
-        this.startStates = startStates;
-        this.finalStates = finalStates;
-    } //конструктор
+    private static class NFABuilder{
+        private int id;
+        private Set<Integer> states;  //състояния
+        private Set<Character> alphabet; //азбука
+        private Map<Integer, Map<Character, Set<Integer>>> transitions; //преходи между състояния
+        private int startStates; //начални състояния
+        private Set<Integer> finalStates; //крайни състояния
 
+        public NFABuilder(int id) {
+            this.id = id;
+        }
 
-    public void addStates(int state){
-        states.add(state);
+        public NFABuilder withStates(Set<Integer> states) {
+            this.states = states;
+            return this;
+        }
+
+        public NFABuilder withAlphabet(Set<Character> alphabet){
+            this.alphabet = alphabet;
+            return this;
+        }
+        public NFABuilder withTransitions(Map<Integer, Map<Character, Set<Integer>>> transitions){
+            this.transitions = transitions;
+            return this;
+        }
+        public NFABuilder withStartStates(int startStates){
+            this.startStates = startStates;
+            return this;
+        }
+        public NFABuilder withFinalStates(Set<Integer> finalStates) {
+            this.finalStates = finalStates;
+            return this;
+        }
+
+        public NFA build(){
+            return new NFA();
+        }
     }
 
-    public void addTransition(int fromState, char ch, int toState){
+    private NFA() {
+    };
 
+    private NFA(NFABuilder builder){
+        this.id = builder.id;
+        this.states = builder.states;
+        this.alphabet = builder.alphabet;
+        this.finalStates = builder.finalStates;
+        this.startStates = builder.startStates;
+        this.transitions = builder.transitions;
     }
+
+
+
 }
